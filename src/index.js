9756dedca913189a544e98bd0a68683b994ae2d5
fujiwo <ffjlabo@gmail.com>
2020-03-30 00:44:59 +0900

    fix: チャット画面の作成

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from "styled-components";
 
-const App = () => <p>Hello World!!</p>;
+import Form from "./components/Form";
+import Logs from "./components/Logs";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  justify-content: center;
+  bottom: 100vh;
+`;
+
+const App = () => (
+  <Wrapper>
+    <Form />
+    <Logs />
+  </Wrapper>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
