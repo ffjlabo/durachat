6b49c771a6a88621fa51ed5061173256a565c1dc
fujiwo <ffjlabo@gmail.com>
2020-04-18 00:16:34 +0900

    fix: アイコン画像の配置位置を変更

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,11 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import styled from "styled-components";
-import io from "socket.io-client";
+import styled from 'styled-components';
+import io from 'socket.io-client';
 
-import Form from "../components/Form";
-import Logs from "../components/Logs";
-
-import icon from "../../assets/img/durachat-icon-02.jpg";
+import Form from '../components/Form';
+import Logs from '../components/Logs';
 
 const Wrapper = styled.div`
   display: flex;
@@ -21,50 +19,50 @@ const MarginForm = styled(Form)`
 const Chat = ({avator}) => {
   const [chatLog, setChatLog] = useState([]);
 
-  const socket = io(":8080");
+  const socket = io(':8080');
 
   const handleSubmit = (text) => {
-    socket.emit("addPost", {avator, text});
+    socket.emit('addPost', {avator, text});
   };
 
   const addPost = (avator, text) => {
     const log = {
-      type: "post",
+      type: 'post',
       content: {
         name: avator.name,
         icon: avator.icon,
-        text
-      }
+        text,
+      },
     };
 
-    setChatLog(prev => [log, ...prev]);
+    setChatLog((prev) => [log, ...prev]);
   };
 
   const addMessage = (text) => {
     const log = {
-      type: "message",
-      content: { text }
+      type: 'message',
+      content: {text},
     };
 
-    setChatLog(prev => [log, ...prev]);
-  }
+    setChatLog((prev) => [log, ...prev]);
+  };
 
   useEffect(() => {
-    socket.on("receivePost", ({avator, text}) => {
+    socket.on('receivePost', ({avator, text}) => {
       addPost(avator, text);
     });
 
-    socket.on("receiveMessage", (text) => {
+    socket.on('receiveMessage', (text) => {
       addMessage(text);
     });
 
-    socket.emit("loginUser", avator.name);
+    socket.emit('loginUser', avator.name);
   }, []);
 
   return (
     <Wrapper>
-      <MarginForm onSubmit={handleSubmit}/>
-      <Logs data={chatLog}/>
+      <MarginForm onSubmit={handleSubmit} />
+      <Logs data={chatLog} />
     </Wrapper>
   );
 };
--- src/utils/iconSetting.js
+++ src/utils/iconSetting.js
@@ -1,128 +1,128 @@
-import img_01 from "../../assets/img/durachat-icon-01.jpg";
-import img_02 from "../../assets/img/durachat-icon-02.jpg";
-import img_03 from "../../assets/img/durachat-icon-03.jpg";
-import img_04 from "../../assets/img/durachat-icon-04.jpg";
-import img_05 from "../../assets/img/durachat-icon-05.jpg";
-import img_06 from "../../assets/img/durachat-icon-06.jpg";
-import img_07 from "../../assets/img/durachat-icon-07.jpg";
-import img_08 from "../../assets/img/durachat-icon-08.jpg";
-import img_09 from "../../assets/img/durachat-icon-09.jpg";
-import img_10 from "../../assets/img/durachat-icon-10.jpg";
-import img_11 from "../../assets/img/durachat-icon-11.jpg";
-import img_12 from "../../assets/img/durachat-icon-12.jpg";
-import img_13 from "../../assets/img/durachat-icon-13.jpg";
-import img_14 from "../../assets/img/durachat-icon-14.jpg";
-import img_15 from "../../assets/img/durachat-icon-15.jpg";
-import img_16 from "../../assets/img/durachat-icon-16.jpg";
-import img_17 from "../../assets/img/durachat-icon-17.jpg";
-import img_18 from "../../assets/img/durachat-icon-18.jpg";
-import img_19 from "../../assets/img/durachat-icon-19.jpg";
-import img_20 from "../../assets/img/durachat-icon-20.jpg";
+import img_01 from '../images/durachat-icon-01.jpg';
+import img_02 from '../images/durachat-icon-02.jpg';
+import img_03 from '../images/durachat-icon-03.jpg';
+import img_04 from '../images/durachat-icon-04.jpg';
+import img_05 from '../images/durachat-icon-05.jpg';
+import img_06 from '../images/durachat-icon-06.jpg';
+import img_07 from '../images/durachat-icon-07.jpg';
+import img_08 from '../images/durachat-icon-08.jpg';
+import img_09 from '../images/durachat-icon-09.jpg';
+import img_10 from '../images/durachat-icon-10.jpg';
+import img_11 from '../images/durachat-icon-11.jpg';
+import img_12 from '../images/durachat-icon-12.jpg';
+import img_13 from '../images/durachat-icon-13.jpg';
+import img_14 from '../images/durachat-icon-14.jpg';
+import img_15 from '../images/durachat-icon-15.jpg';
+import img_16 from '../images/durachat-icon-16.jpg';
+import img_17 from '../images/durachat-icon-17.jpg';
+import img_18 from '../images/durachat-icon-18.jpg';
+import img_19 from '../images/durachat-icon-19.jpg';
+import img_20 from '../images/durachat-icon-20.jpg';
 
 export const iconImages = [
-    img_01,
-    img_02,
-    img_03,
-    img_04,
-    img_05,
-    img_06,
-    img_07,
-    img_08,
-    img_09,
-    img_10,
-    img_11,
-    img_12,
-    img_13,
-    img_14,
-    img_15,
-    img_16,
-    img_17,
-    img_18,
-    img_19,
-    img_20,
+  img_01,
+  img_02,
+  img_03,
+  img_04,
+  img_05,
+  img_06,
+  img_07,
+  img_08,
+  img_09,
+  img_10,
+  img_11,
+  img_12,
+  img_13,
+  img_14,
+  img_15,
+  img_16,
+  img_17,
+  img_18,
+  img_19,
+  img_20,
 ];
 
 export const iconColors = {
-    [img_01]: {
-        main: "#89C15F",
-        sub: "#add490"
-    },
-    [img_02]: {
-        main: "#F66105",
-        sub: "#fb8a44"
-    },
-    [img_03]: {
-        main: "#CACD1E",
-        sub: "#e2e54a"
-    },
-    [img_04]: {
-        main: "#F667BB",
-        sub: "#faa7d8"
-    },
-    [img_05]: {
-        main: "#94065a",
-        sub: "#d50982"
-    },
-    [img_06]: {
-        main: "#262D95",
-        sub: "#343dcb"
-    },
-    [img_07]: {
-        main: "#682908",
-        sub: "#a7420d"
-    },
-    [img_08]: {
-        main: "#FAC711",
-        sub: "#fbd754"
-    },
-    [img_09]: {
-        main: "#46E183",
-        sub: "#80eaaa"
-    },
-    [img_10]: {
-        main: "#6DCF16",
-        sub: "#8feb3e"
-    },
-    [img_11]: {
-        main: "#4A4DDC",
-        sub: "#8385e7"
-    },
-    [img_12]: {
-        main: "#6EB3E6",
-        sub: "#a8d1f0"
-    },
-    [img_13]: {
-        main: "#3C3C3C",
-        sub: "#5e5e5e"
-    },
-    [img_14]: {
-        main: "#95449C",
-        sub: "#b667bd"
-    },
-    [img_15]: {
-        main: "#000000",
-        sub: "#222"
-    },
-    [img_16]: {
-        main: "#103B69",
-        sub: "#195ca4"
-    },
-    [img_17]: {
-        main: "#C60001",
-        sub: "#ff0b0c"
-    },
-    [img_18]: {
-        main: "#2B66FE",
-        sub: "#6f97fe"
-    },
-    [img_19]: {
-        main: "#1E7210",
-        sub: "#2eae18"
-    },
-    [img_20]: {
-        main: "#F1D52E",
-        sub: "#f5e26e"
-    },
+  [img_01]: {
+    main: '#89C15F',
+    sub: '#add490',
+  },
+  [img_02]: {
+    main: '#F66105',
+    sub: '#fb8a44',
+  },
+  [img_03]: {
+    main: '#CACD1E',
+    sub: '#e2e54a',
+  },
+  [img_04]: {
+    main: '#F667BB',
+    sub: '#faa7d8',
+  },
+  [img_05]: {
+    main: '#94065a',
+    sub: '#d50982',
+  },
+  [img_06]: {
+    main: '#262D95',
+    sub: '#343dcb',
+  },
+  [img_07]: {
+    main: '#682908',
+    sub: '#a7420d',
+  },
+  [img_08]: {
+    main: '#FAC711',
+    sub: '#fbd754',
+  },
+  [img_09]: {
+    main: '#46E183',
+    sub: '#80eaaa',
+  },
+  [img_10]: {
+    main: '#6DCF16',
+    sub: '#8feb3e',
+  },
+  [img_11]: {
+    main: '#4A4DDC',
+    sub: '#8385e7',
+  },
+  [img_12]: {
+    main: '#6EB3E6',
+    sub: '#a8d1f0',
+  },
+  [img_13]: {
+    main: '#3C3C3C',
+    sub: '#5e5e5e',
+  },
+  [img_14]: {
+    main: '#95449C',
+    sub: '#b667bd',
+  },
+  [img_15]: {
+    main: '#000000',
+    sub: '#222',
+  },
+  [img_16]: {
+    main: '#103B69',
+    sub: '#195ca4',
+  },
+  [img_17]: {
+    main: '#C60001',
+    sub: '#ff0b0c',
+  },
+  [img_18]: {
+    main: '#2B66FE',
+    sub: '#6f97fe',
+  },
+  [img_19]: {
+    main: '#1E7210',
+    sub: '#2eae18',
+  },
+  [img_20]: {
+    main: '#F1D52E',
+    sub: '#f5e26e',
+  },
 };
 
 export default {iconImages, iconColors};
